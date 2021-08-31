558dffeb91baacce9333e36600e2e3513d3ba7ec
Alex Hester <hester25194@gmail.com>
2021-08-31 13:29:10 +0100

    fix to notification and journey models - datetime

--- db/migrate/20210831112731_create_notifications.rb
+++ db/migrate/20210831112731_create_notifications.rb
@@ -4,8 +4,8 @@ class CreateNotifications < ActiveRecord::Migration[6.0]
       t.string :category
       t.float :latitude
       t.float :longitude
-      t.datetime :notification_start, null: false
-      t.datetime :notification_end, null: false
+      t.datetime :notification_start_date, null: false
+      t.datetime :notification_stop_date, null: false
       t.references :journey, foreign_key: true
       t.text :description
       t.string :image
