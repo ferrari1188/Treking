58736bff62a1832adc51238ddfd236f2444861a8
Megan Luengo <87246548+phenelagayle@users.noreply.github.com>
2021-08-31 17:11:37 +0100

    updated model validations

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,10 +1,8 @@
 class Notification < ApplicationRecord
-  belongs_to :journeys
-  belongs_to :route, through: :journeys
-  belongs_to :user, through: :journeys
+  belongs_to :journey
   validates :category, presence: true
   validates :longitude, presence: true
   validates :latitude, presence: true
-  validates :description, presence: true, length: { minimum: 10 }
+  validates :description, presence: true, length: { minimum: 1 }
   validates :notification_start, presence: true
 end
