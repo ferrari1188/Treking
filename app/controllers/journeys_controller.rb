f39193344ddc94384a2f4aff6cc15075686d905d
Alex Hester <hester25194@gmail.com>
2021-09-01 12:09:03 +0100

    journey controller added - slight change to schema, added completed

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -1,2 +1,44 @@
 class JourneysController < ApplicationController
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
+  def show
+    @journey = Journey.find(params[:id])
+  end
+
+  def new
+    @journey = Journey.new
+  end
+
+  def create
+    @journey = Journey.new(journey_params)
+    @journey.user = current_user
+    @journey.route = @route
+    if @journey.save!
+      redirect_to route_journeys_path(@route, @journey), notice: 'Journey successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @route = Route.find(params[:route_id])
+    @journey = Journey.find(params[:id])
+  end
+
+  def update
+    @route = Route.find(params[:route_id])
+    @journey = Journey.find(params[:id])
+    @journey.user = current_user
+    @journey.route = @route
+    if @journey.save!
+      redirect_to route_journeys_path(@route, @journey), notice: 'Journey successfully completed.'
+    else
+      render :edit
+    end
+  end
+
+  def journey_params
+    params.require(:journey).permit(:journey_start, :journey_end, :completed)
+  end
 end
