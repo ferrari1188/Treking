e25ebc76ee0f2e032356d9f664b94f77ffecfd97
Megan Luengo <87246548+phenelagayle@users.noreply.github.com>
2021-09-01 16:57:16 +0100

    added users seed"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+emails = ['phenela.luengo@gmail.com', 'hester25194@gmail.com', 'chi.ferr.1188@gmail.com', 'benwest0291@outlook.com']
+emails.each do |email|
+  user = User.new(
+    email: email,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: Faker::Team.name.strip,
+    password: "password"
+  )
+  user.save
+end
+
+puts "new users created"
