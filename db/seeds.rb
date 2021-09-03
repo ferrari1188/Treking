0c1af3a95a898ca597913f7138b9a538ccf4a5ec
Megan Luengo <87246548+phenelagayle@users.noreply.github.com>
2021-09-03 16:01:07 +0100

    edited seeds page

--- db/seeds.rb
+++ db/seeds.rb
@@ -100,75 +100,75 @@ new_forest.save
 puts "Routes created"
 
 peak_coordinates = [
- # route1
- {
-   longitude: -1.817374775658692,
-   latitude: 53.364507286303024
-   # coordinates route1 -waypoint1
-  },{
-   longitude: -1.8169447892250332,
-   latitude: 53.37061578983253
-   # coordinates route1 -waypoint2
-  },{
-   longitude: -1.8179850067283212,
-   latitude: 53.3741374461473
-  },{
-   longitude: -1.820757603715208,
-   latitude: 53.37853643846918
-  },{
-   longitude: -1.8224199161511194,
-   latitude: 53.380176859387944
-  },{
-   longitude: -1.838266182156957,
-   latitude: 53.38212656847614
-  },{
-   longitude: -1.8406065328437364,
-   latitude: 53.38196279616014
-  },{
-   longitude: -1.8458643075043142,
-   latitude: 53.38077926311837
-  },{
-   longitude: -1.8587856253182622,
-   latitude: 53.38201178937979
-  },{
-   longitude: -1.8659706946866663,
-   latitude: 53.379789696817454
-  },{
-   longitude: -1.869238028838197,
-   latitude: 53.37939506457417
-  },{
-   longitude: -1.8785678841042,
-   latitude: 53.3781625413061
-  },{
-   longitude: -1.8822132602943782,
-   latitude: 53.37773567728078
-  },{
-   longitude: -1.8820189186178595,
-   latitude: 53.37398745617102
-  },{
-   longitude: -1.871278448332589,
-   latitude: 53.373015281621235
-  },{
-   longitude: -1.8694078545185846,
-   latitude: 53.372724149742936
-  },{
-   longitude: -1.8599735954794312,
-   latitude: 53.36830840594999
-  },{
-   longitude: -1.8550938211750463,
-   latitude: 53.36614889395415
-  },{
-   longitude: -1.8464677741847026,
-   latitude: 53.36485409501344
-  },{
-   longitude: -1.8344742353085337,
-   latitude: 53.366654689457874
-  },{
-   longitude: -1.8168914656147024,
-   latitude: 53.370714015468906
-  },{
-   longitude: -1.8172878097056753,
-   latitude: 53.36448998002262
+  # route1
+  {
+    longitude: -1.817374775658692,
+    latitude: 53.364507286303024
+    # coordinates route1 -waypoint1
+  }, {
+    longitude: -1.8169447892250332,
+    latitude: 53.37061578983253
+    # coordinates route1 -waypoint2
+  }, {
+    longitude: -1.8179850067283212,
+    latitude: 53.3741374461473
+  }, {
+    longitude: -1.820757603715208,
+    latitude: 53.37853643846918
+  }, {
+    longitude: -1.8224199161511194,
+    latitude: 53.380176859387944
+  }, {
+    longitude: -1.838266182156957,
+    latitude: 53.38212656847614
+  }, {
+    longitude: -1.8406065328437364,
+    latitude: 53.38196279616014
+  }, {
+    longitude: -1.8458643075043142,
+    latitude: 53.38077926311837
+  }, {
+    longitude: -1.8587856253182622,
+    latitude: 53.38201178937979
+  }, {
+    longitude: -1.8659706946866663,
+    latitude: 53.379789696817454
+  }, {
+    longitude: -1.869238028838197,
+    latitude: 53.37939506457417
+  }, {
+    longitude: -1.8785678841042,
+    latitude: 53.3781625413061
+  }, {
+    longitude: -1.8822132602943782,
+    latitude: 53.37773567728078
+  }, {
+    longitude: -1.8820189186178595,
+    latitude: 53.37398745617102
+  }, {
+    longitude: -1.871278448332589,
+    latitude: 53.373015281621235
+  }, {
+    longitude: -1.8694078545185846,
+    latitude: 53.372724149742936
+  }, {
+    longitude: -1.8599735954794312,
+    latitude: 53.36830840594999
+  }, {
+    longitude: -1.8550938211750463,
+    latitude: 53.36614889395415
+  }, {
+    longitude: -1.8464677741847026,
+    latitude: 53.36485409501344
+  }, {
+    longitude: -1.8344742353085337,
+    latitude: 53.366654689457874
+  }, {
+    longitude: -1.8168914656147024,
+    latitude: 53.370714015468906
+  }, {
+    longitude: -1.8172878097056753,
+    latitude: 53.36448998002262
   }
 #   #  route2
 #   [{
