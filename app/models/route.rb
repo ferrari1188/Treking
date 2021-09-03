9afc9dba5826d5872a8f5d04c2526d95809898f2
chiara ferrari <chi.ferr1188@gmail.com>
2021-09-03 16:34:51 +0100

    index8

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,6 +5,6 @@ class Route < ApplicationRecord
   has_many :notifications, through: :journey
   validates :description, length: { minimum: 2 }
 
-  geocoded_by :start_location
-  after_validation :geocode, if: :will_save_change_to_start_location?
+  geocoded_by :location_category
+  after_validation :geocode, if: :will_save_change_to_location_category?
 end
