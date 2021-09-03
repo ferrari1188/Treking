60bf7f2ab18439ef3ace0759767a57e336596afe
Megan Luengo <87246548+phenelagayle@users.noreply.github.com>
2021-09-03 12:16:09 +0100

    added new columns to routes

--- db/migrate/20210903104452_add_columns_to_routes.rb
+++ db/migrate/20210903104452_add_columns_to_routes.rb
@@ -1,4 +1,10 @@
 class AddColumnsToRoutes < ActiveRecord::Migration[6.0]
   def change
+    remove_column :routes, :start_location
+    remove_column :routes, :end_location
+    add_column :routes, :route_image_url, :string
+    add_column :routes, :location_category, :string
+    add_column :routes, :time, :string
+    add_column :routes, :distance, :string
   end
 end
