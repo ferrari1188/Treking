d872d0307a4d3a42ebef632cb15090633b2094ec
chiara ferrari <chi.ferr1188@gmail.com>
2021-09-03 16:30:35 +0100

    index8

--- app/models/route.rb
+++ app/models/route.rb
@@ -3,7 +3,7 @@ class Route < ApplicationRecord
   has_many :journeys
   has_many :waypoints
   has_many :notifications, through: :journey
-  validates :start_location, :end_location, presence: true
+  # validates :start_location, :end_location, presence: true
   validates :description, length: { minimum: 2 }
 
   geocoded_by :start_location
