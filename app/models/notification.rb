a571452f20d9a5a516fe340b9091c1d9c9e94396
Megan Luengo <87246548+phenelagayle@users.noreply.github.com>
2021-09-08 12:20:37 +0100

    able to add notifications with cloudinary

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-
+  has_one_attached :photo
   belongs_to :route
   validates :category, presence: true
   validates :description, presence: true, length: { minimum: 1 }
